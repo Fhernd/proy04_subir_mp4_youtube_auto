a65530b06e014f162e95ab94e868a39728e4f43f
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-09-02 09:15:52 -0500

    Adaptando el código para la versión de ejecución con watchdog

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import os
+import sys
+import time
+
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
@@ -8,11 +11,19 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
 
-class GestorGrabacionObs(FileSystemEventHandler):
+class SubidaAutomaticaYouTube(FileSystemEventHandler):
+    def __init__(self):
+        CLIENT_SECRETS_FILE = 'credentials.json'
+        SCOPES = ['https://www.googleapis.com/auth/youtube.upload']
+        API_SERVICE_NAME = 'youtube'
+        API_VERSION = 'v3'
+        
+        self.youtube = self.get_authenticated_service()
+    
     """
     Clase que gestiona los eventos de renombrado de archivos.
     """
-    def on_modified(self, event):
+    def on_created(self, event):
         """
         Método que se ejecuta cuando se modifica un archivo.
         
@@ -24,48 +35,57 @@ class GestorGrabacionObs(FileSystemEventHandler):
         
         nombre_inicial = event.src_path
 
-# Autenticación OAuth 2.0
-def get_authenticated_service():
-    flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
-    credentials = flow.run_local_server()
-    return build(API_SERVICE_NAME, API_VERSION, credentials=credentials)
+        try:
+            self.initialize_upload(self.youtube, event.src_path)  # Reemplaza con la ruta de tu video
+        except HttpError as e:
+            print(f"Ocurrió un error HTTP: {e.resp.status} - {e.content}")
+
+    # Autenticación OAuth 2.0
+    def get_authenticated_service(self):
+        flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
+        credentials = flow.run_local_server()
+        return build(API_SERVICE_NAME, API_VERSION, credentials=credentials)
 
 
-# Subir video
-def initialize_upload(youtube, filename):
-    media_body = MediaFileUpload(filename, chunksize=-1, resumable=True, mimetype='video/mp4')
-    body = {
-        'snippet': {
-            'title': 'Mi video de prueba',
-            'description': 'Este es un video de prueba',
-            'tags': ['test', 'ejemplo'],
-            'categoryId': '22'
-        },
-        'status': {
-            'privacyStatus': 'private'  # o 'public' o 'unlisted'
+    # Subir video
+    def initialize_upload(self, youtube, filename):
+        media_body = MediaFileUpload(filename, chunksize=-1, resumable=True, mimetype='video/mp4')
+        body = {
+            'snippet': {
+                'title': 'Mi video de prueba',
+                'description': 'Este es un video de prueba',
+                'tags': ['test', 'ejemplo'],
+                'categoryId': '22'
+            },
+            'status': {
+                'privacyStatus': 'private'  # o 'public' o 'unlisted'
+            }
         }
-    }
 
-    request = youtube.videos().insert(
-        part=','.join(body.keys()),
-        body=body,
-        media_body=media_body
-    )
+        request = youtube.videos().insert(
+            part=','.join(body.keys()),
+            body=body,
+            media_body=media_body
+        )
 
-    response = None
-    while response is None:
-        status, response = request.next_chunk()
+        response = None
+        while response is None:
+            status, response = request.next_chunk()
 
-    print(f"Subido video con ID: {response['id']}")
+        print(f"Subido video con ID: {response['id']}")
 
-if __name__ == '__main__':
-    CLIENT_SECRETS_FILE = 'credentials.json'
-    SCOPES = ['https://www.googleapis.com/auth/youtube.upload']
-    API_SERVICE_NAME = 'youtube'
-    API_VERSION = 'v3'
+
+if __name__ == "__main__":
+    path = sys.argv[1] if len(sys.argv) > 1 else '.'
     
-    youtube = get_authenticated_service()
+    event_handler = SubidaAutomaticaYouTube()
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+    observer.start()
+    
     try:
-        initialize_upload(youtube, 'video.mp4')  # Reemplaza con la ruta de tu video
-    except HttpError as e:
-        print(f"Ocurrió un error HTTP: {e.resp.status} - {e.content}")
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
